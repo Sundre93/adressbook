79e4d738ef17bbfe529a54dda6099627f38bb889
Sundre93 <63513850+Sundre93@users.noreply.github.com>
2020-04-16 20:29:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 
-
-
 function fillForm()
 {
     document.getElementById("kontakt-form-firstname").value = "Peter";
     document.getElementById("kontakt-form-lastname").value = "Müller";
+    document.getElementById("kontakt-form-email").value = "peter.mueller@gmail.com";
+    document.getElementById("kontakt-form-address").value = "Müllerweg 24";
+    document.getElementById("kontakt-form-stadt").value = "Petersberg";
+    document.getElementById("kontakt-form-plz").value = "12345";
 }
 
 function updateForm() {
-    document.forms.kontakt.FirstName.value ="Peter";
+    document.forms.kontakt.FirstName.value ="";
 }
-
 
