dee56d3490a02c98583e9061cb2ba8b0ec3fa9fe
Sundre93 <63513850+Sundre93@users.noreply.github.com>
2020-04-16 20:17:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+
+function fillForm()
+{
+    document.getElementById("kontakt-form-firstname").value = "Peter";
+    document.getElementById("kontakt-form-lastname").value = "Müller";
+}
+
+function updateForm() {
+    document.forms.kontakt.FirstName.value ="Peter";
+}
+
+
